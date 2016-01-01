01d16798b8d6130add3d5a9835b657a65052cd20
Kevin Fei <kev1fei@gmail.com>
2016-01-01 16:40:53 -0500

    Add project files

---
+++ BuzzerSystem/app/src/main/java/com/example/kevin/buzzersystem/BuzzerActivity.java
@@ -0,0 +1,71 @@
+package com.example.kevin.buzzersystem;
+
+import android.media.Image;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BuzzerActivity extends AppCompatActivity {
+    List<ImageView> buttons = new ArrayList<ImageView>();
+    boolean lockout = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buzzer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        buttons = Arrays.asList((ImageView) findViewById(R.id.button1), (ImageView) findViewById((R.id.button2)), (ImageView) findViewById(R.id.button3), (ImageView) findViewById(R.id.button4));
+    }
+
+    public void pushed(View view) {
+        if (!lockout) {
+            ImageView imageview = (ImageView) view;
+            imageview.setImageResource(R.drawable.pressed);
+            lockout = true;
+        }
+    }
+
+    public void clear(View view) {
+        for (ImageView imageview : buttons) {
+            imageview.setImageResource(R.drawable.unpressed);
+        }
+        lockout = false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_buzzer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
